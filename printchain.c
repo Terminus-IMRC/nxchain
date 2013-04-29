10b87f40c6982e718072a0ef30bb043ed8d631bf
Terminus <i.can.speak.c.and.basic@gmail.com>
2013-04-29 11:42:08 +0900

    Made printchain to print identifier.

--- printchain.c
+++ printchain.c
@@ -1,12 +1,35 @@
 #include "def.h"
+#include <string.h>
 
 void printchain()
 {
 	int i;
+	char s[0xff];
 
 	printf("chaincont: %d\n", chaincont);
 	for(i=0; i<chaincont; i++){
 		printf("#%d: x: %d, y: %d\n", i, ret[i].x, ret[i].y);
+		switch(ret[i].chain){
+			case NAME1:
+				strcpy(s, "NAME1");
+				break;
+			case NAME2:
+				strcpy(s, "NAME2");
+				break;
+			case E1:
+				strcpy(s, "E1");
+				break;
+			case E2:
+				strcpy(s, "E2");
+				break;
+			case E3:
+				strcpy(s, "E3");
+				break;
+			default:
+				will_and_die("printchain: invalid the identifier", 1);
+				break;
+		}
+		printf("\tidentifier: %s\n", s);
 	}
 	return;
 }
